dc51369d3b613b5cf8d8010d0f6d2f502cd0312a
Komei Nomura <komei.nomura@pepabo.com>
2017-06-26 17:49:13 +0900

    Userクラスにfull_nameとalphabetical_nameメソッドを追加

--- example_user.rb
+++ example_user.rb
@@ -2,11 +2,20 @@ class User
   attr_accessor :name, :email
 
   def initialize(attributes = {})
-    @name  = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
     @email = attributes[:email]
   end
 
   def formatted_email
-    "#{@name} <#{@email}>"
+    "#{full_name} <#{@email}>"
+  end
+
+  def full_name
+    "#{@last_name} #{@first_name}"
+  end
+
+  def alphabetical_name
+    "#{@first_name}, #{@last_name}"
   end
 end
