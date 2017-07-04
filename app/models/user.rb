cb6daa06f4f14c00988a1bef4fb9ae6960994ff8
Komei Nomura <komei.nomura@pepabo.com>
2017-07-04 12:53:24 +0900

    メソッドにコメント追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  # 文字列のハッシュを返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
