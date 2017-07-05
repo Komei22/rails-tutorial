0189b6a6d6580103a295d2e7c188e2b181a8ecd9
Komei Nomura <komei.nomura@pepabo.com>
2017-07-05 18:15:45 +0900

    アクセスの仕方によって遷移するページを変更 ログインせずに編集ページにアクセスしようとしたユーザはログイン後編集ページへ

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -47,4 +47,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     assert_redirected_to login_url
   end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user) || default
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
