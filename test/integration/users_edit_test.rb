5699eb81d65726f9a13b640d58c1d1cb200b209e
Komei Nomura <komei.nomura@pepabo.com>
2017-07-07 16:41:19 +0900

    記述を一部修正

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -52,7 +52,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert session[:forwarding_url]
     log_in_as(@user)
-    assert_redirected_to edit_user_url(@user) || default
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
