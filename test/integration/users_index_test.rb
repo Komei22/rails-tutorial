71cc2482d2f690d71eee94a6295a9b8967fdfe16
Komei Nomura <komei.nomura@pepabo.com>
2017-07-10 14:26:33 +0900

    テストの内容を一部修正

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -11,9 +11,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination', count: 2
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      next unless user.activated?
+    assigns(:users).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete'
