8fb265b5a63ffef558221ac0566c39c68dab9792
Komei Nomura <komei.nomura@pepabo.com>
2017-07-11 15:40:40 +0900

    テスト項目を分離

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -6,36 +6,57 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-  test "password resets" do
+  test "invalid email information in password resets new page" do
     get new_password_reset_path
     assert_template 'password_resets/new'
-    # メールアドレスが無効
     post password_resets_path, params: { password_reset: { email: "" } }
     assert_not flash.empty?
     assert_template 'password_resets/new'
-    #メールアドレスが有効
+  end
+
+ test "valid email information in password resets new page" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
     post password_resets_path, params: { password_reset: { email: @user.email } }
     assert_not_equal @user.reset_digest, @user.reload.reset_digest
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirected_to root_url
-    # パスワード再設定フォームテスト
+  end
+
+  test "should redirect to root" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    post password_resets_path, params: { password_reset: { email: @user.email } }
     user = assigns(:user)
-    # メールアドレスが無効
+    # 無効なメールアドレスで要求された時
     get edit_password_reset_path(user.reset_token, email: "")
     assert_redirected_to root_url
-    # 無効なユーザー
+    # 無効なユーザに要求された時
     user.toggle!(:activated)
     get edit_password_reset_path(user.reset_token, email: user.email)
     assert_redirected_to root_url
-    user.toggle!(:activated)
-    # メールアドレスが有効で、トークンが無効
+    # 無効なトークンで要求された時
     get edit_password_reset_path('wrong token', email: user.email)
     assert_redirected_to root_url
-    # メールアドレスもトークンも有効
+  end
+
+  test "redirect to password reset edit page" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    post password_resets_path, params: { password_reset: { email: @user.email } }
+    user = assigns(:user)
     get edit_password_reset_path(user.reset_token, email: user.email)
     assert_template 'password_resets/edit'
     assert_select "input[name=email][type=hidden][value=?]", user.email
+  end
+
+  test "invalid password update information" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    post password_resets_path, params: { password_reset: { email: @user.email } }
+    user = assigns(:user)
+    get edit_password_reset_path(user.reset_token, email: user.email)
     # 無効なパスワードとパスワード確認
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
@@ -48,7 +69,14 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
                     user: { password:              "",
                             password_confirmation: "" } }
     assert_select 'div#error_explanation'
-    # 有効なパスワードとパスワード確認
+  end
+
+  test "valid password update information" do
+    get new_password_reset_path
+    assert_template 'password_resets/new'
+    post password_resets_path, params: { password_reset: { email: @user.email } }
+    user = assigns(:user)
+    get edit_password_reset_path(user.reset_token, email: user.email)
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
                     user: { password:              "foobaz",
