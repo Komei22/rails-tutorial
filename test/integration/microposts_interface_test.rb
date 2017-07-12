1007973533a6b8f9834aa529b2d9f61ae7520d6e
Komei Nomura <komei.nomura@pepabo.com>
2017-07-12 18:58:45 +0900

    画像投稿のテストを追加

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -1,28 +1,41 @@
 require 'test_helper'
-
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
 
-  test "micropost interface" do
+  test "show error message when invalid information" do
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
-    # 無効な送信
+    assert_select 'input[type=file]'
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "" } }
     end
     assert_select 'div#error_explanation'
-    # 有効な送信
+  end
+
+  test "add new micropost when valid information" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     content = "This micropost really ties the room together"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count' do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
-    # 投稿削除する
+  end
+
+  test "delete micropost when own it" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'div.pagination'
+    assert_select 'input[type=file]'
     assert_select 'a', text: 'delete'
     first_micropost = @user.microposts.paginate(page: 1).first
     assert_difference 'Micropost.count', -1 do
