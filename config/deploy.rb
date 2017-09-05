ce8722ab14fadda38842002fe0cb18558ec55101
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-05 14:33:45 +0900

    changed deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "3.9.0"
 
 set :application, "sample_app"
 set :repo_url, "https://github.com/Komei22/sample_app.git"
-set :branch, "master"
+set :branch, "cap-start-puma"
 set :rails_env, 'production'
 
 task :start do
