225dea03414f3442086b34cbbf0a7da72fdb234d
Shohei Ogata <shohei.ogata@pepabo.com>
2017-09-05 15:12:03 +0900

    systemctlコマンド周りを修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,23 +3,23 @@ lock "3.9.0"
 
 set :application, "sample_app"
 set :repo_url, "https://github.com/Komei22/sample_app.git"
-set :branch, "cap-start-puma"
+set :branch, "master"
 set :rails_env, 'production'
 
 task :start do
   on roles(:web) do
-    execute "systemctl start puma"
+    execute "sudo systemctl start puma"
   end
 end
 
 task :stop do
   on roles(:web) do
-    execute "systemctl stop puma"
+    execute "sudo systemctl stop puma"
   end
 end
 
 task :restart do
   on roles(:web) do
-    execute "cd /var/www/sample_app/current/ && bundle exec pumactl restart"
+    execute "sudo systemctl restart puma"
   end
 end
