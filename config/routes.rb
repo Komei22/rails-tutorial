62d0c6ba8e45b5e9719c1145827657ba09b876d5
Komei Nomura <komei.nomura@pepabo.com>
2017-10-04 14:17:52 +0900

    ユーザ情報を取得するAPIのエンドポイントとアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
 
   # ユーザ情報API用のエンドポイントを定義
   scope '/api', { format: 'json' } do
-    resources :users
+    get '/users/:id', to: 'users#show'
+    get '/users/:id/user_info', to: 'users#user_info'
   end
 end
