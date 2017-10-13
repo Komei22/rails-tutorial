6dda59284ebe3ffae868c23081a250ab9d2fad12
Komei Nomura <komei.nomura@pepabo.com>
2017-10-13 18:17:47 +0900

    gravatarのサイズが変更できない問題を修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
   # 引数で与えられたユーザのGravatarを返す
   def gravatar_for(user, size: 80)
-    image_tag(gravatar_url(user), alt: user.name, class: "gravatar")
+    image_tag(gravatar_url(user, size), alt: user.name, class: "gravatar")
   end
 
-  def gravatar_url(user, size: 80)
+  def gravatar_url(user, size)
     id = Digest::MD5::hexdigest(user.email.downcase)
     "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
   end
